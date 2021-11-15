079ec092c1570b3e45d4d62f62f1e13b6cfb7524
Filip Milosevic <milosevic.ra193.2019@uns.ac.rs>
2021-11-15 10:37:11 +0100

    Added FlatLaf library

--- src/main/MyApp.java
+++ src/main/MyApp.java
@@ -1,10 +1,15 @@
 package main;
 
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
 import view.MainFrame;
 
 public class MyApp {
 
 	public static void main(String[] args) {
+		FlatLightLaf.setup();
 		MainFrame mainWindow = new MainFrame();
 		
 	}
