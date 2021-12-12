e74bc380d35c42d47151fdf7fad7786e85aeac34
Stefan Pekez <pekez.ra179.2019@uns.ac.rs>
2021-12-12 18:23:40 +0100

    Added student showcase

---
+++ src/model/DBStudent.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.util.ArrayList;
+
+public class DBStudent {
+	
+	private static DBStudent instance = null;
+	
+	public static DBStudent getInstance() {
+		if(instance == null) {
+			instance = new DBStudent();
+		}
+		return instance;
+	}
+	
+	private ArrayList<Student> students;
+	private ArrayList<String> columns;
+	
+	private DBStudent() {
+		columns = new ArrayList<String>();
+		columns.add("INDEKS");
+		columns.add("IME");
+		columns.add("PREZIME");
+		columns.add("GODINA STUDIJA");
+		columns.add("STATUS");
+		columns.add("PROSEK");
+		
+		initStudents();
+		
+	}
+	
+	private void initStudents() {
+		students = new ArrayList<Student>();
+		
+		students.add(new Student("Pekez", "Stefan", "ra-179-2019", 3, StudentStatus.B, 9.13));
+		students.add(new Student("Milosevic", "Filip", "ra-193-2019", 3, StudentStatus.B, 10.0));
+	}
+	
+	public int getRowCount() {
+		return students.size();
+	}
+	
+	public int getColumnCount() {
+		return 6;
+	}
+	
+	public String getColumnName(int column) {
+		return columns.get(column);
+	}
+	
+	public String getValueAt(int row, int column) {
+		Student stud = students.get(row);
+		
+		switch (column) {
+		case 0:
+			return stud.getIndexNum();
+		case 1:
+			return stud.getName();
+		case 2:
+			return stud.getSurname();
+		case 3:
+			return Integer.toString(stud.getCurrentYearOfStudy());
+		case 4:
+			if(stud.getStatus() == StudentStatus.B) {
+				return "B";
+			}
+			else {
+				return "S";
+			}
+		case 5:
+			return Double.toString(stud.getAvgGrade());
+			
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + column);
+		}
+	}
+	
+}
